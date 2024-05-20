5da5b8cf3108480f8279c6e4eb3f081a934ed589
Haza Nasrullah <hazanasrullah@gmail.com>
2024-05-20 15:33:39 +0700

    initial main

---
+++ lib/peminjaman.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Peminjaman extends StatefulWidget {
+  const Peminjaman({super.key});
+
+  @override
+  State<Peminjaman> createState() => _PeminjamanState();
+}
+
+class _PeminjamanState extends State<Peminjaman> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
