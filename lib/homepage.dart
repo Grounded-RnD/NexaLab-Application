5fae1f471d82b34bc77b03295ac51cad84538ab0
Lordbruh0 <ryohariyono17@gmail.com>
2024-05-20 16:30:13 +0700

    ini home page

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,13 +11,34 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page'),
-      ),
-      body: Container(
-        color: Colors.black,
-        child: Text('ahahha'),
-      )
-    );
+        appBar: AppBar(
+          leading: Icon(
+            Icons.person_pin,
+            size: 35,
+          ),
+          backgroundColor: Colors.black,
+          title: Container(
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.only(left: 15),
+            width: 200,
+            height: 50,
+            color: Color.fromRGBO(66, 66, 66, 100),
+            child: Text(
+              'Hello Ryo\nReady to use lab',
+              style: TextStyle(fontSize: 17),
+            ),
+          ),
+          toolbarHeight: 70,
+          actions: const [
+            Icon(
+              Icons.notifications_rounded,
+              size: 35,
+            )
+          ],
+        ),
+        body: Container(
+          color: Colors.black,
+          child: Text('ahahha'),
+        ));
   }
 }
